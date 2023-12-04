02fc4d31063f275329dd4ba1ac6ebfd3ad38af74
xurshed_2016@mail.ru <xurshed_2016@mail.ru>
2023-12-04 10:32:22 +0300

    finished

---
+++ LinkedListReverse.java
@@ -0,0 +1,42 @@
+public class LinkedListReverse {
+
+    public static ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode current = head;
+
+        while (current != null) {
+            ListNode nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+
+        return prev; // новый головной узел списка
+    }
+
+    public static void printList(ListNode head) {
+        ListNode current = head;
+        while (current != null) {
+            System.out.print(current.val + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        // Пример использования
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+        head.next.next.next.next = new ListNode(5);
+
+        System.out.println("Исходный список:");
+        printList(head);
+
+        head = reverseList(head);
+
+        System.out.println("Список после разворота:");
+        printList(head);
+    }
+}
